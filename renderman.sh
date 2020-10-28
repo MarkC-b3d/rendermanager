710cc30b3243901784c7b58fbfeaa75c4874de35
MarkC-b3d <blendedmarks@gmail.com>
2020-10-28 22:17:06 +0000

    added directory input support

--- renderman.sh
+++ renderman.sh
@@ -1,4 +1,10 @@
+################################################################################
+#Requires user intervention, you need to replace the path to where you have
+#blender installed on your system I use the tarballs because
+#I can specify the exact path
 blenderpath="/home/mac/Blender/blender"
+################################################################################
+
 
 init(){
 clear
@@ -22,7 +28,7 @@ echo ""
 read YNV1
 
 if [ $YNV1 = y ]; then
-  renderimage
+  getDir
 fi
 
 if [ $YNV1 = n ]; then
@@ -30,8 +36,35 @@ if [ $YNV1 = n ]; then
 fi
 }
 
+getDir(){
+  echo ""
+  echo "Where do you want the image saved to?"
+  echo "Please enter full path..."
+  read DIRECTORY
+
+
+  echo ""
+  echo "You entered $DIRECTORY is this correct?"
+  echo ""
+
+  read YNV2
+
+  if [ $YNV2 = y]; then
+    renderimage
+  fi
+
+  if [ $YNV2 = n]; then
+    getDir
+  fi
+
+
+}
+
+#Requires user intervention, you need to replace the path to where you have
+#blender installed on your system I use the tarballs because
+#I can specify the exact path
 renderimage(){
-  $blenderpath -b $BLENDFILE -o /home/mac/render -f 1
+  $blenderpath -b $BLENDFILE -o $DIRECTORY -f 1
 }
 
 
