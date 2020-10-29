6076f29f583cdcefd992e9d1a3fca8fe7c614d78
MarkC-b3d <blendedmarks@gmail.com>
2020-10-29 19:42:36 +0000

    added animation support for cycles

--- renderman.sh
+++ renderman.sh
@@ -8,13 +8,33 @@ blenderpath="/home/mac/Blender/blender"
 
 init(){
 clear
+echo "Animation / Single Image"
+echo ""
+echo "1 = Animation"
+echo "2 = Single Image"
+imageanim
+}
+
+imageanim(){
+read IMAGEANIM
+if [ $IMAGEANIM = 1 ]; then
+animRender
+fi
+
+if [ $IMAGEANIM = 2 ]; then
+imageRender
+fi
+}
+
+animRender(){
+clear
 echo "Where is the blend file you would like rendered?"
 echo ""
 echo "Please enter full path and file extension..."
-getFile
+getFile_anim
 }
 
-getFile(){
+getFile_anim(){
 read BLENDFILE
 
 echo ""
@@ -28,43 +48,97 @@ echo ""
 read YNV1
 
 if [ $YNV1 = y ]; then
-  getDir
+getDir_anim
 fi
 
 if [ $YNV1 = n ]; then
-  init
+animRender
 fi
 }
 
-getDir(){
-  echo ""
-  echo "Where do you want the image saved to?"
-  echo "Please enter full path..."
-  read DIRECTORY
+getDir_anim(){
+echo ""
+echo "Where do you want the image saved to?"
+echo "Please enter full path..."
+read DIRECTORY
 
 
-  echo ""
-  echo "You entered $DIRECTORY is this correct?"
-  echo ""
+echo ""
+echo "You entered $DIRECTORY is this correct?"
+echo ""
 
-  read YNV2
+read YNV2
 
-  if [ $YNV2 = y]; then
-    renderimage
-  fi
+if [ $YNV2 = y ]; then
+renderanim
+fi
 
-  if [ $YNV2 = n]; then
-    getDir
-  fi
+if [ $YNV2 = n ]; then
+getDir_anim
+fi
+}
+
+imageRender(){
+clear
+echo "Where is the blend file you would like rendered?"
+echo ""
+echo "Please enter full path and file extension..."
+getFile_image
+}
+
+getFile_image(){
+read BLENDFILE
+
+echo ""
+echo "You entered $BLENDFILE is this correct?"
+echo ""
+
+echo ""
+echo "Enter y/n"
+echo ""
+
+read YNV1
+
+if [ $YNV1 = y ]; then
+getDir_image
+fi
+
+if [ $YNV1 = n ]; then
+init
+fi
+}
+
+getDir_image(){
+echo ""
+echo "Where do you want the image saved to?"
+echo "Please enter full path..."
+read DIRECTORY
+
+
+echo ""
+echo "You entered $DIRECTORY is this correct?"
+echo ""
+
+read YNV2
+
+if [ $YNV2 = y ]; then
+renderimage
+fi
+
+if [ $YNV2 = n ]; then
+getDir
+fi
 
 
 }
 
-#Requires user intervention, you need to replace the path to where you have
-#blender installed on your system I use the tarballs because
-#I can specify the exact path
+
 renderimage(){
-  $blenderpath -b $BLENDFILE -o $DIRECTORY -f 1
+$blenderpath -b $BLENDFILE -o $DIRECTORY -f 1
+}
+
+renderanim(){
+$blenderpath -b $BLENDFILE -E CYCLES -o $DIRECTORY -a
 }
 
 
